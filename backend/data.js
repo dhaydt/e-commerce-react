a7a95a54d4a6602f79e686e594b104d741b68fe7
dhaydt <1992dayat@gmail.com>
2021-08-03 00:13:07 +0700

    Connect mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Admin",
+      email: "admin@admin.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "Guest",
+      email: "user@user.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       _id: "1",
