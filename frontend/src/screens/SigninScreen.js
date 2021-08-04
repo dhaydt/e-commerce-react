78bc406db0df189b3e20e6c4cbd63f5958d0136c
dhaydt <1992dayat@gmail.com>
2021-08-04 23:06:04 +0700

    SigninScreen added

---
+++ frontend/src/screens/SigninScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function SigninScreen() {
+  return (
+    <div>
+      <form>
+        <div>
+          <h1>Sign In</h1>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter Email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter Password" />
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Sign In
+          </button>
+        </div>
+        <div>
+          <label />
+          <div>
+            New customer? <Link to="/register">Create your Account</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
