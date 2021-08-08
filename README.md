7a353245343ada2026ff77e71583426ac106e1c2
dhaydt <1992dayat@gmail.com>
2021-08-08 19:07:29 +0700

    adding signin and userInfo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-E-Commerce Fullstack
+21. Design SignIn Screen
+    1.create SigninScreen
+    2.render email and password fields
+    3.create signin constants, actions and reducers
+    4.Update Header based on user login
--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-export default function SigninScreen() {
+import { signin } from "../actions/userActions";
+import MessageBox from "../components/MessageBox";
+import LoadingBox from "../components/LoadingBox";
+
+export default function SigninScreen(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/";
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(signin(email, password));
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
+
   return (
     <div>
-      <form className="form">
+      <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Sign In Gazwah Shopify</h1>
         </div>
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Enter Email" required />
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter Email"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="Enter Password" />
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label />
