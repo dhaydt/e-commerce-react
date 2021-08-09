990cc2ed257d058294fb6c836017123b91a4381f
dhaydt <1992dayat@gmail.com>
2021-08-09 20:53:44 +0700

    adding address pages and function

---
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { saveShippingAdrress } from "../actions/cartActions";
+
+import CheckoutSteps from "../components/CheckoutSteps";
+
+export default function ShippingAddressScreen(props) {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  console.log("userr", shippingAddress);
+
+  if (!userInfo) {
+    props.history.push("signin");
+  }
+
+  const [fullName, setFullName] = useState(shippingAddress.fullName);
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      saveShippingAdrress({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
+    );
+    props.history.push("/payment");
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Shipping</h1>
+        </div>
+
+        <div>
+          <label htmlFor="fullName">Full name</label>
+          <input
+            type="text"
+            id="fullName"
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            id="city"
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            type="text"
+            id="postalCode"
+            onChange={(e) => setPostalCode(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            id="country"
+            onChange={(e) => setCountry(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Continue
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
