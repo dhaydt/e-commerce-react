5e36cf1a5c726eb16016cf63672ff0c7f7747ec9
dhaydt <1992dayat@gmail.com>
2021-08-11 01:15:41 +0700

    adding ordered pages and function

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -5,6 +5,19 @@ import { isAuth } from "../utils.js";
 
 const orderRouter = express.Router();
 
+orderRouter.get(
+  "/:id",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: "Order not found" });
+    }
+  })
+);
+
 orderRouter.post(
   "/",
   isAuth,
